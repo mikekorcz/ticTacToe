f5fae5b6410473c990a6f2b7fd9909f0fc281bac
Michal <mkorcz01@gmail.com>
2020-09-01 20:28:33 +0200

    Tic-Tac-Toe game preview

--- src/pl/mkorcz/Main.java
+++ src/pl/mkorcz/Main.java
@@ -1,8 +1,14 @@
 package pl.mkorcz;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("This is example of TicTacToe game board, which I'm going to improve with every commit:");
+        System.out.println();
+        System.out.println("x" + " " + "o" + " " + "x");
+        System.out.println("o" + " " + "x" + " " + "o");
+        System.out.println("x" + " " + "x" + " " + "o");
+        System.out.println();
+        System.out.println("X wins");
     }
 }
